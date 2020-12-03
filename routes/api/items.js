adad9bbfc5ba54222c748d16feaa5fa7d2400a9a
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-03 23:58:30 +0530

    post api create

--- routes/api/items.js
+++ routes/api/items.js
@@ -6,14 +6,27 @@ const router = express.Router();
 //item model
 const Item = require('../../models/Item');
 
-// @route get api/items
-// @desc get all items
-// @acces public
+// @route   get api/items
+// @desc    get all items
+// @acces   public
 
 router.get('/', (req, res) => {
     Item.find()
         .sort({ date: -1})
         .then(items => res.json(items))
+});
+
+// @route   post api/items
+// @desc    create a post
+// @acces   public
+
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save()
+        .then(item => res.json(item));
 });
 
 module.exports = router;
