ca91f5fdcf2a42597b092941418b9efefc337fe2
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-03 23:34:51 +0530

    route api

---
+++ routes/api/items.js
@@ -0,0 +1,19 @@
+//json
+
+const express = require('express');
+const router = express.Router();
+
+//item model
+const Item = require('../../models/Item');
+
+// @route get api/items
+// @desc get all items
+// @acces public
+
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({ date: -1})
+        .then(items => res.json(items))
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
+//bodyparser middleware
 app.use(bodyParser.json());
 
+//db config
 const db = require('./config/key').mongoURI;
 
+
+//mongo connect
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Mongodb connected..'))
     .catch(err => console.log(err));
+
+//use routes
+app.use('/api/items',items);
 
 const port = process.env.PORT || 5000;
 
