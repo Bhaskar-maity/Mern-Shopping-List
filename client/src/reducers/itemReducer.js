3ecf2b0732bf9a866d441716c6ef786ce48e8cc3
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-06 19:12:29 +0530

    item reducer add

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,10 @@
+import uuid from 'uuid';
+
+const initialState = {
+    items: [
+        { id: uuid(), name: 'a'},
+        { id: uuid(), name: 'b'},
+        { id: uuid(), name: 'c'},
+        { id: uuid(), name: 'd'}
+    ]
+}
