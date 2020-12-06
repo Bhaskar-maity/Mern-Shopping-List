118bb16a3f03c60a8c03317522cad82611a2d123
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-06 18:55:28 +0530

    add provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoopingList from './components/ShoppingList';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoopingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoopingList />
+      </div>
+    </Provider>
   );
 }
 
