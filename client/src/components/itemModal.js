e016c11299d8bcd73c4cf63025cddaffb75fd37c
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-07 20:06:01 +0530

    modal button add

---
+++ client/src/components/itemModal.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
+
+import { connect } from 'react-redux';
+import { addItem } from '../actions/itemActions';
+
+class ItemModal extends Component {
+    state = {
+        modal: false,
+        name: ''
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <Button
+                    color="dark"
+                    style={{marginBottom: '2rem'}}
+                    onClick={this.toggle}
+                >Add Item</Button>
+            </div>
+        )
+    }
+
+}
+
+export default connect()(ItemModal);
