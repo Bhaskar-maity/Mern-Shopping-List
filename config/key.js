53614ec0f56dd412966c11ee85932a567a9f59a0
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-08 23:56:00 +0530

    config file setup

--- config/key.js
+++ config/key.js
@@ -1,3 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://bhaskar:bhas123@cluster0.0jh8f.mongodb.net/<dbname>?retryWrites=true&w=majority'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod')
+} else {
+    module.exports = require('./dev')
 }
